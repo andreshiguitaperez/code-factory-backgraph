2e30a10e11cff393e975f3636d84a5ebd2be831b
Nelson Puerta <alcides.puerta@udea.edu.co>
2024-11-06 14:43:10 -0500

    despliegue1

--- src/main/java/com/udea/graphvirtual/config/WebConfig.java
+++ src/main/java/com/udea/graphvirtual/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Permitir todas las rutas
-                .allowedOrigins("http://localhost:3000") // Permitir el origen de tu frontend
+                .allowedOrigins("*") // Permitir el origen de tu frontend
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
                 .allowCredentials(true) // Permitir credenciales si es necesario
                 .allowedHeaders("*"); // Permitir todas las cabeceras
