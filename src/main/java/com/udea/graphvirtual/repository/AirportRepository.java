9aba73693c654e5efdf3fb659a4e006573681581
Neltrin22 <106124779+Neltrin22@users.noreply.github.com>
2024-11-14 22:18:45 -0500

    Delete src/main/java/com/udea/graphvirtual directory

--- src/main/java/com/udea/graphvirtual/repository/AirportRepository.java
+++
@@ -1,9 +0,0 @@
-package com.udea.graphvirtual.repository;
-
-import com.udea.graphvirtual.entity.Airport;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface AirportRepository extends JpaRepository<Airport, UUID> {
-}
